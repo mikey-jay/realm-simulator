c718460066a0d17901452d229e577d0dce25c7a6
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-13 17:59:11 -0400

    craftAndEquipAltar use case feature complete

--- tests/craftAndEquipAltar.test.js
+++ tests/craftAndEquipAltar.test.js
@@ -20,8 +20,43 @@ test('craftAndEquipAltar', (t) => {
     let verse = craftAndEquipAltar(Gotchiverse.addPlayer(Gotchiverse.create(rules), qualifiedPlayer), 0, 0)
     t.equals(verse.players[0].parcels[0].installations.length, 1, 'there is one installation')
     t.equals(verse.players[0].parcels[0].installations[0].type, Altar.create().type, 'there is an installation of type altar')
+    t.equals(verse.players[0].parcels[0].installations[0].width, rules.installations.altar.width, 'has width matching rules')
+    t.equals(verse.players[0].parcels[0].installations[0].height, rules.installations.altar.height, 'has height matching rules')
     t.deepEqual(verse.players[0].tokens, zeroTokens)
     t.throws(() => craftAndEquipAltar(Gotchiverse.addPlayer(Gotchiverse.create(rules), noParcelsPlayer), 0, 0), 'throws if player does not have a parcel')
     t.throws(() => craftAndEquipAltar(Gotchiverse.addPlayer(Gotchiverse.create(rules), noMoneyPlayer), 0, 0), 'throws if player does not have enough funds')
+
+    verse.rules.installations.altar.maxQuantityPerParcel = {
+        'humble': 1,
+        'reasonable': 1,
+        'spacious': 1,
+        'partner': 1
+    }
+
+    verse.players[0] = Player.addTokens(verse.players[0], rules.installations.altar.buildCosts[0])
+
+    t.throws(() => craftAndEquipAltar(verse, 0, 0), 'throw if try to build more than the maximum allowed ')
+
+
+    verse.rules.installations.altar.maxQuantityPerParcel = {
+        'humble': undefined,
+        'reasonable': undefined,
+        'spacious': undefined,
+        'partner': undefined
+    }
+
+    t.doesNotThrow(() => craftAndEquipAltar(verse, 0, 0), 'do not throw if not maximum defined')
+
+    let rulesInstantBuild = structuredClone(rules)
+    rulesInstantBuild.installations.altar.buildTime[0] = 0
+    let verseInstantBuild = craftAndEquipAltar(Gotchiverse.addPlayer(Gotchiverse.create(rulesInstantBuild), qualifiedPlayer), 0, 0)
+    t.equals(verseInstantBuild.players[0].parcels[0].installations[0].buildTimeRemaining, rulesInstantBuild.installations.altar.buildTime[0], 'build time is correct (no build time)')
+    t.equals(verseInstantBuild.players[0].parcels[0].installations[0].level, 1, 'instant build should advance immediately to level 1')
+
+    let rulesWithBuildTime = structuredClone(rules)
+    rulesWithBuildTime.installations.altar.buildTime[0] = 267846
+    let verseWithBuildTime = craftAndEquipAltar(Gotchiverse.addPlayer(Gotchiverse.create(rulesWithBuildTime), qualifiedPlayer), 0, 0)
+    t.equals(verseWithBuildTime.players[0].parcels[0].installations[0].buildTimeRemaining, rulesWithBuildTime.installations.altar.buildTime[0], 'build time is correct (has build time)')
+
     t.end();
 });
--- use_cases/craftAndEquipAltar.js
+++ use_cases/craftAndEquipAltar.js
@@ -1,9 +1,20 @@
 const Altar = require('../entities/altar.js')
 const Parcel = require('../entities/parcel.js')
+const Player = require('../entities/player.js')
 
 module.exports = function (gotchiverseIn, playerIndex, parcelIndex) {
-    let gotchiverseOut = { ...gotchiverseIn }
+    let gotchiverseOut = structuredClone(gotchiverseIn)
     let a = Altar.create()
+    let parcelSize = gotchiverseOut.players[playerIndex].parcels[parcelIndex].size
+    let maxAltarsAllowed = gotchiverseOut.rules.installations[a.type].maxQuantityPerParcel[parcelSize]
+    if ((typeof maxAltarsAllowed != 'undefined') && Parcel.getInstallationTypeCount(gotchiverseOut.players[playerIndex].parcels[parcelIndex], a.type) >= maxAltarsAllowed)
+        throw new Error (`maximum installations of type ${a.type} has been reached for this parcel`)
+    a.width = gotchiverseOut.rules.installations[a.type].width
+    a.height = gotchiverseOut.rules.installations[a.type].height
+    a = Altar.addBuildTime(a, gotchiverseOut.rules.installations.altar.buildTime[0])
+    if (a.buildTimeRemaining <= 0)
+        a.level = a.buildLevel
     gotchiverseOut.players[playerIndex].parcels[parcelIndex] = Parcel.addInstallation(gotchiverseIn.players[playerIndex].parcels[parcelIndex], a)
+    gotchiverseOut.players[playerIndex] = Player.removeTokens(gotchiverseOut.players[playerIndex], gotchiverseOut.rules.installations.altar.buildCosts[0])
     return gotchiverseOut
 }
