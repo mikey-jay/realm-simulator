1b757389dd818ca1c2c8acbd3abf7f7dbb8b91a4
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-13 15:17:46 -0400

    changed spread operator to structuredClone, removed transaction functionality

--- entities/wallet.js
+++ entities/wallet.js
@@ -1,47 +1,37 @@
 function create () {
-    return { tokens: {}, parcels: [], installations: [], transactions: [], installationsRemoved: [], parcelsRemoved: [] }
+    return { tokens: {}, parcels: [], installations: [] }
 }
 
 function addSingleToken (walletIn, token, qty) {
-    let walletOut = {...walletIn}
+    let walletOut = structuredClone(walletIn)
     if (Object.keys(walletOut.tokens).includes(token))
         walletOut.tokens[token] += qty
     else
         walletOut.tokens[token] = qty
     if (walletOut.tokens[token] < 0) throw new Error(`insufficient ${token} token supply`)
-    let tx = getCurrentTransaction(walletOut)
-    if (tx) tx = addSingleToken(tx, token, qty)
     return walletOut
 }
 
 function addSingleOrMultipleTokens (walletIn, tokens, qtyOrMultiplier = 1) {
-    if (typeof tokens == 'string') return addSingleToken (walletIn, tokens, qtyOrMultiplier)
-    let walletOut = {...walletIn}
+    let walletOut = structuredClone(walletIn)
+    if (typeof tokens == 'string') return addSingleToken (walletOut, tokens, qtyOrMultiplier)
     for (t in tokens)
         walletOut = addSingleToken(walletOut, t, tokens[t] * qtyOrMultiplier)
     return walletOut
 }
 
-function getCurrentTransaction(wallet) {
-    if (typeof wallet.transactions == 'undefined') return
-    return wallet.transactions[wallet.transactions.length - 1]
-}
-
 function addNFT(walletIn, type, nft) {
-    const walletOut = {...walletIn}
+    let walletOut = structuredClone(walletIn)
     walletOut[type].push(nft)
-    let tx = getCurrentTransaction(walletOut)
-    if (tx) tx = addNFT(tx, type, nft)
     return walletOut    
 }
 
 function removeNFT (walletIn, type, index) {
-    let walletOut = {...walletIn}
-    const removedType = `${type}Removed`
+    let walletOut = structuredClone(walletIn)
     if (index > walletIn[type].length - 1) throw new Error('invalid index (too high)')
     if (index < 0) throw new Error('invalid index (too low)')
     let [removedNFT] = walletOut[type].splice(index, 1)
-    return addNFT(walletOut, removedType, removedNFT)
+    return walletOut
 }
 
 function addInstallation (parcelIn, installation) {
@@ -64,12 +54,6 @@ function removeTokens (walletIn, token, qty = 1) {
     return addSingleOrMultipleTokens(walletIn, token, qty * -1)
 }
 
-function startTransaction(walletIn) {
-    let walletOut = {...walletIn}
-    walletOut.transactions.push(create())
-    return walletOut
-}
-
 module.exports = {
     create,
     addTokens: addSingleOrMultipleTokens,
@@ -77,6 +61,5 @@ module.exports = {
     addInstallation,
     removeInstallation,
     addParcel,
-    removeParcel,
-    startTransaction
+    removeParcel
 }
--- tests/wallet.test.js
+++ tests/wallet.test.js
@@ -3,38 +3,13 @@ const Wallet = require('../entities/wallet.js')
 
 test('Tape is working', (t) => { t.true(true); t.end(); });
 
-test('Wallet.startTransaction', (t) => {
-    const w = Wallet.create()
-    t.equal(w.transactions.length, 0, 'new wallet has no transactions')
-    const w2 = Wallet.startTransaction(w)
-    t.equal(w2.transactions.length, 1, 'has a single transaction')
-    const w3 = Wallet.addTokens(w2, 'a_token', 10)
-    t.equal(w3.transactions.length, 1, 'still has a single transaction')
-    const w4 = Wallet.addTokens(w2, 'a_token', 20)
-    t.equal(w4.transactions[0].tokens.a_token, 30, 'transaction totals tokens')
-    const w5 = Wallet.addTokens(Wallet.startTransaction(w4), 'b_token', 50)
-    t.equal(w5.transactions[1].tokens.b_token, 50, 'new transaction has the new token')
-    t.equal(typeof w5.transactions[1].tokens.a_token, 'undefined', 'new transaction does not have the old token')
-    const i = { type: 'altar' }
-    const w6 = Wallet.removeInstallation(Wallet.addInstallation(w5, i), 0)
-    t.deepEqual(w6.transactions[1].installationsRemoved[0], i, 'installations removed appear in installationsRemoved')
-    t.end()
-})
-
-test('Wallet.addTokens', (t) => {
-    const w = Wallet.create()
-    const tokens = { token1: 10, token2: 20 }
-    const w2 = Wallet.addTokens(w, tokens)
-    t.deepEqual(w2.tokens, tokens, 'all tokens were added')
-    t.end()
-})
-
-test('Wallet.removeTokens', (t) => {
+test('Wallet.addTokens, Wallet.removeTokens', (t) => {
     const w = Wallet.create()
     const tokens = { token1: 10, token2: 20 }
     const noTokens = { token1: 0, token2: 0 }
     const w2 = Wallet.addTokens(w, tokens)
-    const w3 = Wallet.removeTokens(w, tokens)
-    t.deepEqual(w3.tokens, noTokens, 'all tokens were removed')
+    t.deepEqual(w2.tokens, tokens, 'all tokens were added')
+    t.deepEqual(Wallet.removeTokens(w2, tokens).tokens, noTokens, 'all tokens were removed')
+    t.deepEqual(w2.tokens, tokens, 'no side effects')
     t.end()
 })
