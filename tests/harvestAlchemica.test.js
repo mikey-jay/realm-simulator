5b450147c06a9a4bdbb7976428d48291e4f565e7
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-17 11:40:34 -0400

    harvestAlchemica harvests all gotchiverse alchemica

--- tests/harvestAlchemica.test.js
+++ tests/harvestAlchemica.test.js
@@ -1,5 +1,5 @@
 const test = require('tape');
-const { harvestAlchemica } = require('../use_cases/harvestAlchemica.js')
+const { harvestWithSingleHarvester, harvestAlchemica } = require('../use_cases/harvestAlchemica.js')
 const { pipe } = require('../utils.js')
 const Harvester = require('../entities/harvester.js')
 const Reservoir = require('../entities/reservoir.js')
@@ -7,7 +7,7 @@ const Parcel = require('../entities/parcel.js')
 const Player = require('../entities/player.js')
 const Gotchiverse = require('../entities/gotchiverse.js')
 
-test('harvestAlchemica', (t) => {
+test('harvestWithSingleHarvester', (t) => {
     const rules = require('../rulesets/testRules.js')
 
     const fudHarvesterL1 = pipe(Harvester.create('fud'), Harvester.addLevel)
@@ -22,28 +22,60 @@ test('harvestAlchemica', (t) => {
     const qualifiedPlayer = pipe(Player.create(), [Player.addParcel, qualifiedParcel])
 
     // if the installation passed is not a harvester, throws
-    t.throws(() => pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, qualifiedPlayer], [harvestAlchemica, 0, 0, 1]), 'throws if the installation is not a harvester')
+    t.throws(() => pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, qualifiedPlayer], [harvestWithSingleHarvester, 0, 0, 1]), 'throws if the installation is not a harvester')
 
     // set the rules so that the reservoir can only hold one day of fud
     rules.installations.reservoir_fud.capacities[0] = rules.installations.harvester_fud.harvestRates[0]
   
-    const oneDayResult = pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, qualifiedPlayer], [harvestAlchemica, 0, 0, 0, oneDayInBlocks])
+    const oneDayResult = pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, qualifiedPlayer], [harvestWithSingleHarvester, 0, 0, 0, oneDayInBlocks])
     t.equals(oneDayResult.players[0].parcels[0].tokens.fud, startingFudInParcel - rules.installations.harvester_fud.harvestRates[0], 'harvesting a full day takes a full harvest rate from the parcel')
-    const noReservoirsResult = pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, noReservoirsPlayer], [harvestAlchemica, 0, 0, 0, oneDayInBlocks])
+    const noReservoirsResult = pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, noReservoirsPlayer], [harvestWithSingleHarvester, 0, 0, 0, oneDayInBlocks])
     t.equals(noReservoirsResult.players[0].parcels[0].tokens.fud, startingFudInParcel, 'harvesting with no reservoirs removes no alchemica from the parcel')
-    const halfDayResult = pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, qualifiedPlayer], [harvestAlchemica, 0, 0, 0, oneDayInBlocks / 2])
+    const halfDayResult = pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, qualifiedPlayer], [harvestWithSingleHarvester, 0, 0, 0, oneDayInBlocks / 2])
     t.equals(halfDayResult.players[0].parcels[0].tokens.fud, startingFudInParcel - rules.installations.harvester_fud.harvestRates[0] / 2, 'harvesting a half day takes a half day of harvest rate from the parcel')
     t.equals(halfDayResult.players[0].parcels[0].installations[1].tokens.fud, rules.installations.harvester_fud.harvestRates[0] / 2, 'harvested alchemica is in the reservoir')
     t.deepEqual(halfDayResult.players[0].parcels[0].installations[2].tokens, {}, 'second reservoir is empty')
 
-    const twoDayResult = harvestAlchemica(oneDayResult, 0, 0, 0, oneDayInBlocks)
+    const twoDayResult = harvestWithSingleHarvester(oneDayResult, 0, 0, 0, oneDayInBlocks)
     t.equals(twoDayResult.players[0].parcels[0].installations[1].tokens.fud, rules.installations.reservoir_fud.capacities[0], 'first reservoir has not changed')
     t.equals(twoDayResult.players[0].parcels[0].installations[2].tokens.fud, rules.installations.harvester_fud.harvestRates[0], 'second reservoir is full on the second day')
 
-    const threeDayResult = harvestAlchemica(twoDayResult, 0, 0, 0, oneDayInBlocks)
+    const threeDayResult = harvestWithSingleHarvester(twoDayResult, 0, 0, 0, oneDayInBlocks)
     t.equals(threeDayResult.players[0].parcels[0].tokens.fud, 0, 'third day - cannot harvest more alchemica than is in the parcel')
     t.equals(threeDayResult.players[0].parcels[0].installations[3].tokens.fud, rules.installations.harvester_fud.harvestRates[0] /  2, 'third reservoir is only half full on the third day - alchemica has run out')
 
     t.end()
 
+})
+
+test('harvestAlchemica', (t) => {
+    const rules = require('../rulesets/testRules.js')
+
+    const fudHarvesterL1 = pipe(Harvester.create('fud'), Harvester.addLevel)
+    const fudReservoirL1 = pipe(Reservoir.create('fud'), Reservoir.addLevel)
+    const alphaHarvesterL1 = pipe(Harvester.create('alpha'), Harvester.addLevel)
+    const alphaReservoirL1 = pipe(Reservoir.create('alpha'), Reservoir.addLevel)
+
+    // set the rules so that the reservoir can only hold one day of fud
+    rules.installations.reservoir_fud.capacities[0] = rules.installations.harvester_fud.harvestRates[0]
+    rules.installations.reservoir_alpha.capacities[0] = rules.installations.harvester_alpha.harvestRates[0]
+
+    const startingFudInParcel = rules.installations.harvester_fud.harvestRates[0] * 2.5 // enough for 2 day test - runs out on 3rd day
+    const startingAlphaInParcel = rules.installations.harvester_alpha.harvestRates[0] * 2.5 // enough for 2 day test - runs out on 3rd day
+    const oneDayInBlocks = 60 * 60 * 24 / rules.secondsPerBlock
+
+    const qualifiedParcel = pipe(Parcel.create('spacious'), [Parcel.addAlchemica, 'fud', startingFudInParcel], [Parcel.addAlchemica, 'alpha', startingAlphaInParcel], [Parcel.addInstallation, fudHarvesterL1], [Parcel.addInstallation, alphaHarvesterL1], [Parcel.addInstallation, fudReservoirL1], [Parcel.addInstallation, alphaReservoirL1])
+    const qualifiedPlayer = pipe(Player.create(), [Player.addParcel, qualifiedParcel], [Player.addParcel, qualifiedParcel])
+    const qualifiedPlayer2 = pipe(Player.create(), [Player.addParcel, qualifiedParcel])
+
+    // set the rules so that the reservoir can only hold one day of fud
+    rules.installations.reservoir_fud.capacities[0] = rules.installations.harvester_fud.harvestRates[0]
+
+    const oneDayResult = pipe(Gotchiverse.create(rules), [Gotchiverse.addPlayer, qualifiedPlayer], [Gotchiverse.addPlayer, qualifiedPlayer2], [harvestAlchemica, oneDayInBlocks])
+    t.equals(oneDayResult.players[0].parcels[0].tokens.fud, startingFudInParcel - rules.installations.harvester_fud.harvestRates[0], 'fud harvesters have harvested')
+    t.equals(oneDayResult.players[0].parcels[0].tokens.alpha, startingAlphaInParcel - rules.installations.harvester_alpha.harvestRates[0], 'alpha harvesters have harvested')
+    t.equals(oneDayResult.players[0].parcels[1].tokens.fud, startingFudInParcel - rules.installations.harvester_fud.harvestRates[0], 'second parcel was harvested')
+    t.equals(oneDayResult.players[1].parcels[0].tokens.fud, startingFudInParcel - rules.installations.harvester_fud.harvestRates[0], 'second player\'s parcel was harvested')
+
+    t.end()
 })
--- use_cases/harvestAlchemica.js
+++ use_cases/harvestAlchemica.js
@@ -2,7 +2,7 @@ const Parcel = require('../entities/parcel.js')
 const Reservoir = require('../entities/reservoir.js')
 
 
-function harvestAlchemica(gotchiverseIn, playerIndex, parcelIndex, installationIndex, blockCount) {
+function harvestWithSingleHarvester(gotchiverseIn, playerIndex, parcelIndex, installationIndex, durationInBlocks) {
     const gotchiverseOut = structuredClone(gotchiverseIn)
     const playerParcel = gotchiverseOut.players[playerIndex].parcels[parcelIndex]
     const playerHarvester = playerParcel.installations[installationIndex]
@@ -13,7 +13,7 @@ function harvestAlchemica(gotchiverseIn, playerIndex, parcelIndex, installationI
     if (playerHarvester.class != 'harvester') throw new Error('Can only harvest alchemica from harvesters')
 
     const harvestRate = rules.installations[playerHarvester.type].harvestRates[playerHarvester.level - 1]
-    let harvestQtyRemaining = harvestRate * (blockCount / oneDayInBlocks)
+    let harvestQtyRemaining = harvestRate * (durationInBlocks / oneDayInBlocks)
 
     for (let i = 0 ; i < gotchiverseOut.players[playerIndex].parcels[parcelIndex].installations.length && harvestQtyRemaining > 0 ; i++) {
         if (gotchiverseOut.players[playerIndex].parcels[parcelIndex].installations[i].type == useReservoirType) {
@@ -30,6 +30,26 @@ function harvestAlchemica(gotchiverseIn, playerIndex, parcelIndex, installationI
     return gotchiverseOut
 }
 
+function harvestAlchemicaFromParcel(gotchiverseIn, playerIndex, parcelIndex, durationInBlocks) {
+    let gotchiverseOut = structuredClone(gotchiverseIn)
+    Parcel.getInstallationClassIndexes(gotchiverseOut.players[playerIndex].parcels[parcelIndex], 'harvester').forEach((installationIndex) => {
+        gotchiverseOut = harvestWithSingleHarvester(gotchiverseOut, playerIndex, parcelIndex, installationIndex, durationInBlocks)
+    })
+    return gotchiverseOut
+}
+
+function harvestAlchemica(gotchiverseIn, durationInBlocks) {
+    let gotchiverseOut = structuredClone(gotchiverseIn)
+    for (let playerIndex = 0 ; playerIndex < gotchiverseOut.players.length ; playerIndex++) {
+        for (let parcelIndex = 0 ; parcelIndex < gotchiverseOut.players[playerIndex].parcels.length ; parcelIndex++) {
+            gotchiverseOut = harvestAlchemicaFromParcel(gotchiverseOut, playerIndex, parcelIndex, durationInBlocks)
+        }
+    }
+    return gotchiverseOut
+}
+
 module.exports = {
+    harvestWithSingleHarvester,
+    harvestAlchemicaFromParcel,
     harvestAlchemica
 }
