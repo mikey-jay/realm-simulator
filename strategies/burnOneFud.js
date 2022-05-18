3aa1ee3d0d349619c09464b0354ce0df6ed21287
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-18 15:12:19 -0400

    return a factory

--- strategies/burnOneFud.js
+++ strategies/burnOneFud.js
@@ -1,6 +1,14 @@
 const Player = require('../entities/player.js')
-module.exports = function burnOneFud (verseIn, playerIndex) {
+const Result = require('../entities/player.js')
+
+module.exports = function burnOneFud (verseIn, playerIndex, parcelIndex) {
+    if (Player.getTokenBalance(verseIn.players[playerIndex], 'fud') >= 1)
+        return burnFud
+    return false
+}
+
+const burnFud = (verseIn, playerIndex, parcelIndex) => {
     const verseOut = structuredClone(verseIn)
-    verseOut.players[playerIndex] = Player.removeTokens(verseOut.players[playerIndex], 1, 'fud')
+    verseOut.players[playerIndex] = Player.removeTokens(verseOut.players[playerIndex], 'fud', 1)
     return verseOut
 }
