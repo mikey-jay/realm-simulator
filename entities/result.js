ca94baa156505c1b802df7e29c8a865d1b7b0c08
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-18 15:12:03 -0400

    calculate playerTotals on creation

--- entities/result.js
+++ entities/result.js
@@ -1,6 +1,12 @@
+const Player = require('../entities/player.js')
 
-function create(gotchiverse, playerIndex, useCaseName) {
-    return { blockTime: gotchiverse.currentTime, playerIndex, useCaseName, playerTotals: { tokens: {}, parcels: {}, installations: {}} }
+function create(gotchiverse, playerIndex, parcelIndex, useCaseName) {
+    const playerTotals = {
+        tokens: structuredClone(gotchiverse.players[playerIndex].tokens),
+        parcels: Player.getTotalParcels(gotchiverse.players[playerIndex]),
+        installations: Player.getTotalInstallations(gotchiverse.players[playerIndex])
+    }
+    return { blockTime: gotchiverse.currentTime, playerIndex, parcelIndex, useCaseName, playerTotals }
 }
 
 module.exports = {
--- tests/result.test.js
+++ tests/result.test.js
@@ -1,15 +1,18 @@
 const test = require('tape');
 const Result = require('../entities/result.js')
 const Gotchiverse = require('../entities/gotchiverse.js')
+const Player = require('../entities/player.js')
+const Parcel = require('../entities/player.js')
 const { pipe } = require('../utils.js')
 
 test('Result.create', (t) => {
-    let verse = pipe(Gotchiverse.create(), [Gotchiverse.addTime, 100])
-    const result = Result.create(verse, 2, 'a_use_case')
+    const qualifiedPlayer = pipe(Player.create(), [Player.addParcel, Parcel.create('spacious')])
+    let verse = pipe(Gotchiverse.create(), [Gotchiverse.addTime, 100], [Gotchiverse.addPlayer, qualifiedPlayer])
+    const result = Result.create(verse, 0, 0, 'a_use_case')
     t.equals(result.blockTime, verse.currentTime, 'result has a block time')
-    t.equals(result.playerIndex, 2, 'result has playerIndex')
+    t.equals(result.playerIndex, 0, 'result has playerIndex')
+    t.equals(result.parcelIndex, 0, 'result has playerIndex')
     t.equals(result.useCaseName, 'a_use_case', 'result has use case')
-
     t.end()
 })
 
