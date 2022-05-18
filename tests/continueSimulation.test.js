279d35ac3f45429c8c68a2f71b10801f36c52930
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-18 19:56:39 -0400

    pass time and record emptyReservoir results

--- tests/continueSimulation.test.js
+++ tests/continueSimulation.test.js
@@ -1,14 +1,10 @@
 const test = require('tape');
-const Installation = require('../entities/installation.js')
 const Simulation = require('../entities/simulation.js')
 const {continueSimulation} = require('../use_cases/continueSimulation.js')
 const {startSimulation} = require('../use_cases/startSimulation.js')
-
 const Parcel = require('../entities/parcel.js')
-const Player = require('../entities/player.js')
 const Bot = require('../entities/bot.js')
 const Gotchiverse = require('../entities/gotchiverse.js')
-const Wallet = require('../entities/wallet.js')
 const { pipe } = require('../utils.js')
 
 test('continueSimulation', (t) => {
@@ -16,10 +12,9 @@ test('continueSimulation', (t) => {
     const sim = pipe(Simulation.create('testRules'), [Simulation.addBot, burnFudBot], [Simulation.addBot, burnFudBot], startSimulation, continueSimulation)
     t.equals(Gotchiverse.getTokenBalance(sim.gotchiverse.players[0], 'fud'), 0, 'All fud has been burned by player 1')
     t.equals(Gotchiverse.getTokenBalance(sim.gotchiverse.players[1], 'fud'), 0, 'All fud has been burned by player 2')
-    t.equals(sim.results.length, 6, 'there should have been 6 actions taken')
-    t.equals(sim.results[0].blockTime, sim.gotchiverse.currentTime, 'block time matches result')
-    t.equals(sim.results[0].useCaseName, 'burnFud', 'block time matches result')
-    t.equals(sim.results[0].playerTotals.tokens.fud, 2, 'result shows 2 fud after first action')
-    t.equals(sim.results[5].playerTotals.tokens.fud, 0, 'result shows 0 fud after sixth action')
+    t.equals(sim.results.filter((r) => r.useCaseName == 'burnFud').length, 6, 'there should have been 6 actions taken')
+    t.equals(sim.results[0].blockTime, sim.passTimeBlocks, 'block time matches result')
+    t.equals(sim.results.filter((r) => r.useCaseName == 'burnFud')[0].playerTotals.tokens.fud, 2, 'result shows 2 fud after first action')
+    t.equals(sim.results.filter((r) => r.useCaseName == 'burnFud')[5].playerTotals.tokens.fud, 0, 'result shows 0 fud after sixth action')
     t.end()
 })
--- use_cases/continueSimulation.js
+++ use_cases/continueSimulation.js
@@ -1,22 +1,25 @@
-const Gotchiverse = require("../entities/gotchiverse.js")
 const Simulation = require("../entities/simulation.js")
 const Result = require("../entities/result.js")
-
+const {passTime} = require('./passTime.js')
 
 function continueSimulation(simulationIn) {
     let simulationOut = structuredClone(simulationIn)
     for (let bi = 0 ; bi < simulationOut.bots.length ; bi++) {
         for (let pi = 0 ; pi < simulationOut.gotchiverse.players[bi].parcels.length ; pi++) {
+            const beforeStrategyResult = Result.create(simulationOut.gotchiverse, bi, pi, 'emptyReservoirs')
+            simulationOut = Simulation.addResult(simulationOut, beforeStrategyResult)
             const strategyFactory = require(`../strategies/${simulationOut.bots[bi].strategyName}`)
             let strategy = strategyFactory(simulationOut.gotchiverse, bi, pi)
             while (typeof strategy == 'function') {
                 simulationOut.gotchiverse = strategy(simulationOut.gotchiverse, bi, pi)
-                const result = Result.create(simulationOut.gotchiverse, bi, pi, strategy.name)
-                simulationOut = Simulation.addResult(simulationOut, result)
+                const afterStrategyResult = Result.create(simulationOut.gotchiverse, bi, pi, strategy.name)
+                simulationOut = Simulation.addResult(simulationOut, afterStrategyResult)
                 strategy = strategyFactory(simulationOut.gotchiverse, bi, pi)
             } 
         }
     }
+    if (simulationOut.gotchiverse.currentTime < simulationOut.endTime)
+        simulationOut.gotchiverse = passTime(simulationOut.gotchiverse, simulationOut.passTimeBlocks)
     return simulationOut
 }
 
