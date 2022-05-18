069437e6e95146a5a935756a01c43b47df5ea23b
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-18 11:35:25 -0400

    add playerTotals property

--- entities/result.js
+++ entities/result.js
@@ -1,6 +1,6 @@
 
 function create(gotchiverse, playerIndex, useCaseName) {
-    return { blockTime: gotchiverse.currentTime, playerIndex, useCaseName }
+    return { blockTime: gotchiverse.currentTime, playerIndex, useCaseName, playerTotals: { tokens: {}, parcels: {}, installations: {}} }
 }
 
 module.exports = {
