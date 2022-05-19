7e082b6b3f7cb001fb5718a73d0bd6067af9ffa7
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-19 13:03:22 -0400

    use case defaults to empty string

--- entities/result.js
+++ entities/result.js
@@ -2,7 +2,7 @@ const Player = require('../entities/player.js')
 const Parcel = require('../entities/parcel.js')
 const {subtractObjectKeys} = require('../utils.js')
 
-function create(gotchiverse, playerIndex, parcelIndex, useCaseName, lastResult = { playerTotals: {}, parcelTotals: {} }) {
+function create(gotchiverse, playerIndex, parcelIndex, useCaseName = '', lastResult = { playerTotals: {}, parcelTotals: {} }) {
     const playerTotals = {
         tokens: structuredClone(gotchiverse.players[playerIndex].tokens),
         parcels: Player.getTotalParcels(gotchiverse.players[playerIndex]),
