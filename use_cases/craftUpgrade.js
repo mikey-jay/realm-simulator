1eeb3fa27661570490fe7149e8a894a0e3c28d4f
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-19 21:37:36 -0400

    refactor, add upgradeHighestLevel... functions

--- use_cases/craftUpgrade.js
+++ use_cases/craftUpgrade.js
@@ -7,16 +7,12 @@ function craftUpgrade(gotchiverseIn, playerIndex, parcelIndex, installationIndex
     const installationType = gotchiverseOut.players[playerIndex].parcels[parcelIndex].installations[installationIndex].type
     const buildLevel = ++gotchiverseOut.players[playerIndex].parcels[parcelIndex].installations[installationIndex].buildLevel
     const currentLevel = gotchiverseOut.players[playerIndex].parcels[parcelIndex].installations[installationIndex].level
-    const levelPrerequisite = gotchiverseOut.rules.installations[installationType].levelPrerequisite
     const buildTime = gotchiverseOut.rules.installations[installationType].buildTime[buildLevel - 1]
 
     if (buildLevel > currentLevel + 1) throw new Error('Can\'t upgrade while an upgrade is already in progress')
     if (buildLevel > 9) throw new Error('Can\'t upgrade - maximum installation level of 9 reached')
 
-    if (typeof levelPrerequisite != 'undefined') {
-        const maxLevel = Math.max(0,...gotchiverseOut.players[playerIndex].parcels[parcelIndex].installations.filter((i) => levelPrerequisite == i.type).map((i) => i.level))
-        if (buildLevel > maxLevel) throw new Error(`${installationType} cannot exceed maximum level of the highest level ${levelPrerequisite}`)
-    }
+    if (buildLevel > getMaxLevelOfInstallationType(gotchiverseIn, playerIndex, parcelIndex, installationType)) throw new Error(`${installationType} cannot exceed maximum level of the highest level ${levelPrerequisite}`)
 
     let maxConcurrentUpgrades = gotchiverseOut.rules.maxConcurrentUpgrades
     if (typeof maxConcurrentUpgrades != 'undefined') {
@@ -46,6 +42,11 @@ function upgradeLowestLevelInstallationOfType(gotchiverseIn, playerIndex, parcel
     return craftUpgrade(gotchiverseIn, playerIndex, parcelIndex, Parcel.getIndexOfLowestLevelInstallation(playerParcel, installationType))
 }
 
+function upgradeHighestLevelInstallationOfType(gotchiverseIn, playerIndex, parcelIndex, installationType) {
+    const playerParcel = gotchiverseIn.players[playerIndex].parcels[parcelIndex]
+    return craftUpgrade(gotchiverseIn, playerIndex, parcelIndex, Parcel.getIndexOfHighestLevelInstallation(playerParcel, installationType))
+}
+
 const upgradeLowestLevelFudHarvester = (...args) => upgradeLowestLevelInstallationOfType(...args, 'harvester_fud')
 const upgradeLowestLevelFomoHarvester = (...args) => upgradeLowestLevelInstallationOfType(...args, 'harvester_fomo')
 const upgradeLowestLevelAlphaHarvester = (...args) => upgradeLowestLevelInstallationOfType(...args, 'harvester_alpha')
@@ -56,6 +57,16 @@ const upgradeLowestLevelAlphaReservoir = (...args) => upgradeLowestLevelInstalla
 const upgradeLowestLevelKekReservoir = (...args) => upgradeLowestLevelInstallationOfType(...args, 'reservoir_kek')
 const upgradeLowestLevelMaker = (...args) => upgradeLowestLevelInstallationOfType(...args, 'maker')
 const upgradeLowestLevelAltar = (...args) => upgradeLowestLevelInstallationOfType(...args, 'altar')
+const upgradeHighestLevelFudHarvester = (...args) => upgradeHighestLevelInstallationOfType(...args, 'harvester_fud')
+const upgradeHighestLevelFomoHarvester = (...args) => upgradeHighestLevelInstallationOfType(...args, 'harvester_fomo')
+const upgradeHighestLevelAlphaHarvester = (...args) => upgradeHighestLevelInstallationOfType(...args, 'harvester_alpha')
+const upgradeHighestLevelKekHarvester = (...args) => upgradeHighestLevelInstallationOfType(...args, 'harvester_kek')
+const upgradeHighestLevelFudReservoir = (...args) => upgradeHighestLevelInstallationOfType(...args, 'reservoir_fud')
+const upgradeHighestLevelFomoReservoir = (...args) => upgradeHighestLevelInstallationOfType(...args, 'reservoir_fomo')
+const upgradeHighestLevelAlphaReservoir = (...args) => upgradeHighestLevelInstallationOfType(...args, 'reservoir_alpha')
+const upgradeHighestLevelKekReservoir = (...args) => upgradeHighestLevelInstallationOfType(...args, 'reservoir_kek')
+const upgradeHighestLevelMaker = (...args) => upgradeHighestLevelInstallationOfType(...args, 'maker')
+const upgradeHighestLevelAltar = (...args) => upgradeHighestLevelInstallationOfType(...args, 'altar')
 
 function levelUpAllCompletedUpgrades(gotchiverseIn) {
     let gotchiverseOut = structuredClone(gotchiverseIn)
@@ -67,6 +78,14 @@ function levelUpAllCompletedUpgrades(gotchiverseIn) {
         }
     }
     return gotchiverseOut
+}
+
+function getMaxLevelOfInstallationType(gotchiverseIn, playerIndex, parcelIndex, installationType) {
+    const levelPrerequisite = gotchiverseIn.rules.installations[installationType].levelPrerequisite
+
+    if (typeof levelPrerequisite == 'undefined') return gotchiverseIn.rules.installations[installationType].maxLevel
+
+    return Math.max(0,...gotchiverseIn.players[playerIndex].parcels[parcelIndex].installations.filter((i) => levelPrerequisite == i.type).map((i) => i.level))
 }
 
 module.exports = {
@@ -83,5 +102,17 @@ module.exports = {
    upgradeLowestLevelAlphaReservoir,
    upgradeLowestLevelKekReservoir,
    upgradeLowestLevelAltar,
-   upgradeLowestLevelMaker
+   upgradeLowestLevelMaker,
+   upgradeHighestLevelInstallationOfType,
+    upgradeHighestLevelFudHarvester,
+    upgradeHighestLevelFomoHarvester,
+    upgradeHighestLevelAlphaHarvester,
+    upgradeHighestLevelKekHarvester,
+    upgradeHighestLevelFudReservoir,
+    upgradeHighestLevelFomoReservoir,
+    upgradeHighestLevelAlphaReservoir,
+    upgradeHighestLevelKekReservoir,
+    upgradeHighestLevelAltar,
+    upgradeHighestLevelMaker,
+   getMaxLevelOfInstallationType
 }
