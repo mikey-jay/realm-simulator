8cd4c867865a9e5a402d92f09db5f82ef74221ba
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-25 13:06:44 -0400

    update strategy for async execution

--- simulations/makerLimitsHarvesterQty.js
+++ simulations/makerLimitsHarvesterQty.js
@@ -1,13 +1,5 @@
-const Simulation = require('../entities/simulation.js')
-const {runSimulation} = require('../use_cases/runSimulation.js')
-const Parcel = require('../entities/parcel.js')
-const Bot = require('../entities/bot.js')
-const { pipe } = require('../utils.js')
-
+const runStrategyAsync = require('../use_cases/runStrategyAsync.js')
 const rulesetName = 'makerLimitsHarvesterQty'
-const rules = require(`../rulesets/${rulesetName}.js`)
+const strategies = ['expandHorizontal', 'expandVertical']
 
-const startingAlchemica = rules.avgBaseAlchemicaPerParcel.spacious
-const horizontalBot = pipe(Bot.create('expandHorizontal'), [Bot.addTokens, startingAlchemica], [Bot.addParcel, Parcel.create('spacious')])
-const verticalBot = pipe(Bot.create('expandVertical'), [Bot.addTokens, startingAlchemica], [Bot.addParcel, Parcel.create('spacious')])
-module.exports = (harvestFrequencyHrs, endTimeDays) => pipe(Simulation.create(rulesetName, harvestFrequencyHrs, endTimeDays), [Simulation.addBot, horizontalBot], [Simulation.addBot, verticalBot], runSimulation)
+module.exports = (harvestFrequencyHrs, endTimeDays) => runStrategyAsync(strategies, rulesetName, harvestFrequencyHrs, endTimeDays)
