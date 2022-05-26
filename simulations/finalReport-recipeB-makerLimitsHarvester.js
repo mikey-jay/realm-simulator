9d881500bedd07563b50af88a76790f53c3366c3
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-05-26 18:35:50 -0400

    add simulations for final report

--- simulations/finalReport-recipeB-makerLimitsHarvester.js
+++ simulations/finalReport-recipeB-makerLimitsHarvester.js
@@ -1,5 +1,5 @@
 const runStrategyAsync = require('../use_cases/runStrategyAsync.js')
-const rulesetName = 'current'
+const rulesetName = 'recipeBAndMakerLimitsHarvester'
 const strategies = ['expandVertical', 'expandHorizontal', 'horizontalToL1', 'onlyL1FudHarvesters']
 
 module.exports = (...args) => runStrategyAsync(strategies, rulesetName, ...args)
