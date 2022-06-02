08e211d1d23dcf9e1aedd787f5986827397a7e27
Mikey Jay <65405097+mikey-jay@users.noreply.github.com>
2022-06-02 19:19:36 -0400

    include harvest rates of harvester upgrades in progress when calculating harvest rate (fixes abundance not taking into account current upgrades)

--- use_cases/getTotalHarvestRates.js
+++ use_cases/getTotalHarvestRates.js
@@ -2,10 +2,10 @@ const Parcel = require('../entities/parcel.js')
 const { addObjectKeys } = require('../utils.js')
 
 function getTotalHarvestRates(verseIn, playerIndex, parcelIndex) {
-    const harvesters = Parcel.getInstallationsOfClass(verseIn.players[playerIndex].parcels[parcelIndex], 'harvester').filter((h) => h.level > 0)
+    const harvesters = Parcel.getInstallationsOfClass(verseIn.players[playerIndex].parcels[parcelIndex], 'harvester').filter((h) => h.buildLevel > 0)
     const getHarvestRate = (h) => {
         let rate = { fud: 0, fomo: 0, alpha: 0, kek: 0 }
-        rate[h.resourceToken] = verseIn.rules.installations[h.type].harvestRates[h.level - 1]
+        rate[h.resourceToken] = verseIn.rules.installations[h.type].harvestRates[h.buildLevel - 1]
         return rate
     }
     const harvestRates = harvesters.map(getHarvestRate)
